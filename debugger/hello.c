3be2fc3dd15b64fbd4e1d220a321f5c858a6afe8
ivartb <ivart07@mail.ru>
2017-05-04 14:35:39 +0300

    Memory fixed

--- debugger/hello.c
+++ debugger/hello.c
@@ -1,6 +1,14 @@
+#include <stdio.h>
+
 int main()
 {
-    sleep(10);
-    write(1, "Hello, world!\n", 14);
+    int a = 3;
+    int b = a + 42;
+    printf("%lld\n", (size_t)&a);
+    printf("%p\n", &a);
+    for (int i = 0; i < 3; i++) {
+        int c = a + b - i;
+    }
+    //     write(1, "Hello, world!\n", 14);
     return 239;
 }
